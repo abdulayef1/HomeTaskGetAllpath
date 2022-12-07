2a998bf5c92cb2bc3d577a142f784c3978ecc72d
ibramila <70vq7w9@code.edu.az>
2022-12-07 11:47:42 +0400

    created

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC");
+File.Create(@"C:\Users\ClassTime\Desktop\NewDC\text.txt");
+
+Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File1");
+Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File2");
+Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File2\NestedFile");
+
+var allPaths=Directory.GetDirectories(@"C:\Users\ClassTime\Desktop\NewDC");
+
+foreach (var path in allPaths)
+{
+    Console.WriteLine(path);
+}
