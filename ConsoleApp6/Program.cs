c9d29cbd68ee9c07fa63dfde36f5055cc1ac1cb7
abdulayef1 <7ka1cy@.code.edu.az>
2022-12-08 07:44:17 +0400

    changes saved

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,14 +1,35 @@
 ﻿
-Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC");
-File.Create(@"C:\Users\ClassTime\Desktop\NewDC\text.txt");
+//Directory.CreateDirectory(@"C:\Users\Мухаммед\Desktop\Test");
+//File.Create(@"C:\Users\Мухаммед\Desktop\Test\text.txt");
+//Directory.CreateDirectory(@"C:\Users\Мухаммед\Desktop\Test\File1");
+//Directory.CreateDirectory(@"C:\Users\Мухаммед\Desktop\Test\File2");
+//Directory.CreateDirectory(@"C:\Users\Мухаммед\Desktop\Test\File2\NestedFile");
 
-Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File1");
-Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File2");
-Directory.CreateDirectory(@"C:\Users\ClassTime\Desktop\NewDC\File2\NestedFile");
 
-var allPaths=Directory.GetDirectories(@"C:\Users\ClassTime\Desktop\NewDC");
+ShowAllPath(@"C:\Users\Мухаммед\Desktop\Test");
 
-foreach (var path in allPaths)
+void ShowAllPath(string directory)
 {
-    Console.WriteLine(path);
+    string[] directories = Directory.GetDirectories(directory);
+    foreach (var path in directories)
+    {
+        Console.WriteLine(path);
+        ShowAllFiles(path); 
+        if (Directory.GetDirectories(path).Length != 0)
+        {
+            ShowAllPath((string)path);
+        }
+    }
+}
+
+void ShowAllFiles(string directory)
+{
+    string[] files = Directory.GetFiles(directory);
+    if (files.Length != 0)
+    {
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+        }
+    }
 }
